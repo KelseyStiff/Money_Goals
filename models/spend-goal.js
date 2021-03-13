c1c806e765f176be2fd9a0ab8b163bb356cfef4f
kelseystiff <kelsey>
2021-03-13 17:06:34 -0600

    delete table item on click added

---
+++ models/spend-goal.js
@@ -0,0 +1,20 @@
+//defines database columns and data types for spending goal database
+module.exports = (sequelize, DataTypes) => {
+
+    let SpendGoal = sequelize.define('SpendGoal', {
+        month: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        amount: {
+            type: DataTypes.NUMBER,
+            allowNull: false
+        }
+    })
+
+    SpendGoal.sync({force: true}).then( () => {
+        console.log('synced spending goals table')
+    })
+
+    return SpendGoal
+}
